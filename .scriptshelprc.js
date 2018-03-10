87e3c3e0f3c5f2e8f6f2fe4c125d7cb1823c8774
Decebal Dobrica (Deci) <decebal1988@gmail.com>
2018-03-10 00:27:59 +0000

    remove codeception setup

--- .scriptshelprc.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-    "watch": {
-        "Description": "Starts watchers for composer and unit tests",
-        "Usage": "npm run watch",
-        "Long Description" : [
-        ]
-    },
-};
